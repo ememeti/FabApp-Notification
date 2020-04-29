73b7c44c65205bd5676880332a674e4d1f507825
jkerich1 <49698549+jkerich1@users.noreply.github.com>
2020-04-28 21:43:29 -0500

    Remove Exakat
    Removed Exakat files

--- exakat/projects/fabapp/.exakat/dump-434787cc7e9c7040.php
+++
@@ -1,8 +0,0 @@
-<?php $queries = array (
-  0 => 'DELETE FROM hashResults WHERE name = \'Class Children\'',
-  1 => 'INSERT INTO hashResults ("name", "key", "value") VALUES (\'Class Children\', \'0\', \'44\') 
-, (\'Class Children\', \'1\', \'3\') 
-, (\'Class Children\', \'3\', \'1\') 
-, (\'Class Children\', \'4\', \'1\') 
-',
-); ?>

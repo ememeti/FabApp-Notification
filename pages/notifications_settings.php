838a0233351b34eeb794787a1595199c5c9a5ae6
ememeti <43897389+ememeti@users.noreply.github.com>
2020-05-02 16:12:58 -0500

    Revert "added changes"

--- pages/notifications_settings.php
+++ pages/notifications_settings.php
@@ -36,7 +36,7 @@ $device_mats = Materials::getDeviceMats(2);
     </div>
     <!-- /.row 1 end -->
 
-      <!-- /.row 2 start when sign in -->
+    <!-- /.row 2 start when sign in -->
     <div class="row">
         <!-- /.col-md-8 start -->
         <div class="col-md-8">
@@ -63,80 +63,48 @@ $device_mats = Materials::getDeviceMats(2);
                                 Email
                             </label>
                         </div>
+                        <div>
+                            <h6>Receive Alerts When:</h6>
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Change in Wait Queue Position
+                            </label>
+
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Change in Ticket Balance
+                            </label>
+                        </div>
+
+                        <div>
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Change in 3rd Print Status
+                            </label>
+
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                On Ticket Creation
+                            </label>
+                        </div>
 
                         <div>
                             <h6>Alert Receiving Information: </h6>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                            </div>
-                            <div class="form-group">
-                                <label for="phone">Phone number:</label>
-                                <div>
-                                    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12">
-                                <small>Format: 123-456-7890</small>
+                            <form>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                 </div>
-                                
-                            </div>
-                            <div class="form-group" form method="post">
+                                <div class="form-group">
+                                    <label for="phone">Phone number:</label>
+                                    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12">
+                                    <small>Format: 123-456-7890</small>
+                                </div>
 
-
-                                <?php
-                                if (isset($_POST['sendmail'])) {
-                                    require '../vendor/phpmailer/PHPMailerAutoload.php';
-
-                                    define('EMAIL', ''); // add personal details of the sender's email
-                                    define('PASS', ''); // add personal details of the sender's email password
-
-                                    $mail = new PHPMailer;
-
-                                    $mail->SMTPDebug = 4;                                 // Enable verbose debug output
-
-                                    $mail->isSMTP();                                      // Set mailer to use SMTP
-                                    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
-                                    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-                                    $mail->Username = EMAIL;                              // SMTP username
-                                    $mail->Password = PASS;                               // SMTP password
-                                    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-                                    $mail->Port = 587;                                   // TCP port to connect to
-
-                                    $mail->setFrom(EMAIL, 'Email Test');
-                                    //$mail->addAddress($_POST['exampleInputEmail1']);     // Add a recipient
-                                    $mail->addAddress('memeti.endrit@gmail.com');
-
-                                    $mail->addReplyTo(EMAIL);
-
-                                    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-                                    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-                                    $mail->isHTML(true);                                  // Set email format to HTML
-
-                                    $mail->Subject = 'Here is the subject';
-                                    $mail->Body    = '<div style="border:2px solid red;"This is the HTML message body <b>in bold!</b></div>';
-                                    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-                                    if (!$mail->send()) {
-                                        echo 'Message could not be sent.';
-                                        echo 'Mailer Error: ' . $mail->ErrorInfo;
-                                    } else {
-                                        echo 'Message has been sent';
-                                    }
-                                }
-                                ?>
-
-                                <a href="/pages/notifications.php">
-                                    <input type="submit" name="sendmail" class="btn btn-lg btn-success btn-block pull-right" value="Save" />
-                                </a>
-                                <hr><hr><hr>
-                                <a href="/pages/notifications.php">
-                                    <input type="submit" name="sendmail" class="btn btn-lg btn-success btn-block pull-right" value="Go Back" />
-
-                                </a>
-
-
-
-                            </div>
-
+                                <a class="btn btn-primary pull-right" href="/pages/notifications.php" role="button">Go Back</a>
+                                <a class="btn btn-primary pull-right" href="/pages/notifications.php" role="button">Save</a>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -148,7 +116,57 @@ $device_mats = Materials::getDeviceMats(2);
             <!-- panel end -->
         </div>
         <!-- /.col-md-8 -->
-</div>
+    </div>
+    <div class="row">
+        <div class="col-sm-9 form-group">
+            <form method="post"> 
+                <?php
+                    if(isset($_POST['sendmail'])) {
+                        // require '../vendor/phpmailer/PHPMailerAutoload.php';
+
+                        // define('EMAIL', ''); // add personal details of the sender's email
+                        // define('PASS', ''); // add personal details of the sender's email password
+
+                        // $mail = new PHPMailer;
+
+                        // $mail->SMTPDebug = 4;                                 // Enable verbose debug output
+
+                        // $mail->isSMTP();                                      // Set mailer to use SMTP
+                        // $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+                        // $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                        // $mail->Username = EMAIL;                              // SMTP username
+                        // $mail->Password = PASS;                               // SMTP password
+                        // $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                        // $mail->Port = 587;                                   // TCP port to connect to
+
+                        // $mail->setFrom(EMAIL, 'Email Test');
+                        // //$mail->addAddress($_POST['exampleInputEmail1']);     // Add a recipient
+                        // $mail->addAddress('memeti.endrit@gmail.com');
+
+                        // $mail->addReplyTo(EMAIL);
+
+                        // //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+                        // //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+                        // $mail->isHTML(true);                                  // Set email format to HTML
+
+                        // $mail->Subject = 'Here is the subject';
+                        // $mail->Body    = '<div style="border:2px solid red;"This is the HTML message body <b>in bold!</b></div>';
+                        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                        // if(!$mail->send()) {
+                        //     echo 'Message could not be sent.';
+                        //     echo 'Mailer Error: ' . $mail->ErrorInfo;
+                        // } else {
+                        //     echo 'Message has been sent';
+                        // }
+
+                        Notifications::sendNotification('1564', 'Test Subject', 'Test Message', 1);
+                    }
+                ?>
+                <input type="submit" name="sendmail" class="btn btn-lg btn-success btn-block pull-right" value="Send Mail"/>
+            </form>
+        </div>
+    </div>
 </div>
 <!-- /#page-wrapper end -->
 

1142cb7b99b37a4550117316f63e2027d7c48681
jkerich1 <49698549+jkerich1@users.noreply.github.com>
2020-05-03 18:10:14 -0500

    Added alert notification
    Alerts now can send notification through information on an alert entry, also can remove all alerts with a certain operator

--- class/Alerts.php
+++ class/Alerts.php
@@ -8,6 +8,7 @@ class Alerts{
 
     public static function newAlert($operator, $d_id, $phone, $carrier, $email){
         global $mysqli;
+        //add regex
 
         if ($mysqli->query("
                 INSERT INTO alerts 
@@ -16,7 +17,7 @@ class Alerts{
                     ('$d_id', '$operator',CURRENT_TIMESTAMP, '$email', '$phone', '$carrier');
             ")){        
                 
-                return $mysqli->insert_id;
+                return; //$mysqli->insert_id;
             } else {
                 return ("<div class='alert alert-danger'>Error updating alert info!</div>");
             }
@@ -32,14 +33,14 @@ class Alerts{
         "))
         {
 
-        return $mysqli->insert_id;
+        return; //$mysqli->insert_id;
         } else {
             return ("<div class='alert alert-danger'>Error Connecting Transaction to Alert</div>");
         }
 
     }
 
-    public static function removeAlert($trans_id){
+    public static function removeAlertWiithTrans($trans_id){
         global $mysqli;
 
         if ($result = $mysqli->query("
@@ -48,11 +49,69 @@ class Alerts{
         "))
         {
 
-        return $mysqli->insert_id;
+        return; //$mysqli->insert_id;
         } else {
             return ("<div class='alert alert-danger'>Error Deleting Alert</div>");
         }
 
+    }
+
+    public static function removeAlertWithOp($operator){
+        global $mysqli;
+
+        if ($result = $mysqli->query("
+            DELETE FROM `alerts`
+                WHERE `Operator` = $operator;
+        "))
+        {
+
+        return; //$mysqli->insert_id;
+        } else {
+            return ("<div class='alert alert-danger'>Error Deleting Alert</div>");
+        }
+
+    }
+
+
+    public static function sendAlertWithTran($trans_id, $subject, $message){
+        global $mysqli;
+        
+        // This function queries the carrier table and sends an email to all combinations
+
+        //Query the phone number and email
+        if ($result = $mysqli->query("
+            SELECT `Op_phone` AS `Phone`, `Op_email` AS `Email`, `carrier` AS `Provider` 
+            FROM `alerts`
+            WHERE `t_id` = $trans_id
+        ")) 
+        {
+            $row = $result->fetch_assoc();
+            $phone = $row['Phone'];
+            $email = $row['Email'];
+            $provider = $row['Provider'];
+
+        
+            if (!empty($phone)) {
+                if ($result = $mysqli->query("
+                    SELECT `email`
+                    FROM `carrier`
+                    WHERE `provider` = '$provider';
+                ")) {
+                    while ($row = $result->fetch_assoc()) {
+                        list($a, $b) = explode('number', $row['email']);
+
+                        Notifications::SendMail("".$phone."".$b."", $subject, $message);
+                    }
+                } else {
+                    echo("Carrier query failed!");
+                }
+            }
+            
+            if (!empty($email)) {
+                Notifications::SendMail($email, $subject, $message);
+            }
+    
+        }
     }
 }
 ?>
